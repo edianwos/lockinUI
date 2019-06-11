ea97a70061e63369b20e52ca6705704eea977874
Jinhoon Jeong <edianwos@gmail.com>
2019-06-11 19:31:19 +0900

    first working local client

--- lockinUI/driver.py
+++ lockinUI/driver.py
@@ -1,6 +1,7 @@
 import visa
 from random import random
 
+
 class LockIn:
     def __init__(self, inst, **kwargs):
         self.inst = inst
@@ -15,42 +16,52 @@ class LockIn:
 
     @property
     def frequency(self):
-        return self._frequency
+        try:
+            return self._frequency
+        except AttributeError:
+            return float(self.inst.query("SOUR:FREQ?"))
 
     @frequency.setter
     def frequency(self, value):
-        resp = self.inst.write("FREQ {:7E}".format(value))
+        resp = self.inst.write("SOUR:FREQ {:7E}".format(value))
         if resp[1] == 0:
             self._frequency = value
         else:
-            self._frequency = float(self.inst.query("FREQ?"))
+            raise Exception
 
     @property
     def TC(self):
-        return self._TC
-    
+        try:
+            return self._TC
+        except AttributeError:
+            return float(self.inst.query("FILT:TCON?"))
+
     @TC.setter
     def TC(self, value):
-        resp = self.inst.write("FILT:TCON {value:7E}")
+        resp = self.inst.write("FILT:TCON {:7E}".format(value))
         if resp[1] == 0:
             self._TC = value
         else:
-            self._TC = float(self.inst.query("FILT:TCON?"))
+            raise Exception
 
     @property
     def output(self):
-        return self._output
+        return float(self.inst.query("SOUR:VOLT?"))
+        # return self._output
 
     @output.setter
     def output(self, value):
-        resp = self.inst.write("FILT:TCON {value:7E}")
+        resp = self.inst.write("SOUR:VOLT {}".format(value))
         if resp[1] == 0:
             self._output = value
         else:
-            self._output = float(self.inst.query("SOUR:VOLT ?"))
+            raise Exception
 
     def fetch(self):
         return self.inst.query("FETC?").strip()
+
+    def close(self):
+        self.inst.close()
 
 
 def fake_measure():
@@ -58,8 +69,7 @@ def fake_measure():
     return "{},{}".format(random(), random())
 
 
-"""
-inst.write("INP:COU AC") # or DC
+list_of_commands = """inst.write("INP:COU AC") # or DC
 inst.write("INPUT:FILTER:NOTCH1 ON") # or OF, 1, 2
 inst.write("INPUT:FILTER:NOTCH2 ON") # or OF, 1, 2
 inst.write("INPUT:IMPedance 1E6") # or 50
@@ -69,9 +79,17 @@ inst.write("FILTer:SLOPe 24") #{6|12|18|24}
 
 inst.write("FILTer:TCONstant 1") #<time constant in sec> 
 inst.query("FETCH?")
-inst.write("FREQ {:7E}".format(freq)) #1.234567E+05 
+inst.write("SOUR:FREQ {:7E}".format(freq)) #1.234567E+05 
 inst.write("SOURce:VOLTage <amplitude> )
 {numeric, range 0.00000 to 1.000, setting resolution 4 digits
 (at output voltage range full scale), unit Vrms} Suffix M (10-3),
- unit  V, MAX, and  MIN can be used.  Example: 100M (= 0.1) 
-"""
+ unit  V, MAX, and  MIN can be used.  Example: 100M (= 0.1)"""
+
+
+if __name__ == "__main__":
+    rm = visa.ResourceManager()
+    inst = LockIn(rm.open_resource('GPIB0::1::INSTR'))
+
+    inst.frequency = 2000
+    inst.inst.query("FREQ?")
+    inst.inst.write("FREQ 3000")
--- lockinUI/measurement_routine.py
+++ lockinUI/measurement_routine.py
@@ -5,12 +5,12 @@ Point = namedtuple("Point", ['frequency', 'TC', 'ST'])
 
 def linear_scan(start, end, num=11, step=0, TC=0, ST=0, average=1):
     span = end - start
+    if step == 0:
+        step = span / (num-1)
     if TC == 0:
         TC = 1 / step
     if ST == 0:
         ST = TC * 5
-    if step == 0:
-        step = span / (num-1)
     freq = start
     for i in range(num):
         for j in range(average):
